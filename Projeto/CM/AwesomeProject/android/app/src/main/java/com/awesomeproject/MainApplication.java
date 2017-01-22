b05eeffe3ff81864af30b3925d97470b4870da47
Ricardo Morais <moraispgsi@gmail.com>
2017-01-22 18:04:57 +0000

    Svg a funcionar

--- Projeto/CM/AwesomeProject/android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ Projeto/CM/AwesomeProject/android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -4,18 +4,14 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.horcrux.svg.SvgPackage;
-import com.horcrux.svg.SvgPackage;
-import com.horcrux.svg.SvgPackage;
-import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
 import java.util.Arrays;
 import java.util.List;
+import com.horcrux.svg.SvgPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -29,10 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new SvgPackage(),
-            new SvgPackage(),
-            new SvgPackage(),
-            new SvgPackage()
+           new SvgPackage()
       );
     }
   };
